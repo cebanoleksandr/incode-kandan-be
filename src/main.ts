21cbaf5592d7cc151bcb411f5e290b16b314494d
Oleksandr Cheban <cebanoleksandr@gmail.com>
2024-03-27 12:49:56 +0200

    commit

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,10 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  try {
-    const app = await NestFactory.create(AppModule);
-    app.enableCors();
-    app.useGlobalPipes(new ValidationPipe())
-    await app.listen(3005);
-  } catch (error) {
-    console.log(error);    
-  }
+  const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  app.useGlobalPipes(new ValidationPipe())
+  await app.listen(3005);
 }
 bootstrap();
 export default bootstrap;
