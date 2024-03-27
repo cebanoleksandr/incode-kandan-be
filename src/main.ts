8ae2c0828161870990d689e15dbcb9881fc7f1b3
Oleksandr Cheban <cebanoleksandr@gmail.com>
2024-03-27 12:16:09 +0200

    do export default bootstrap function

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
-export async function bootstrap() {
+async function bootstrap() {
   try {
     const app = await NestFactory.create(AppModule);
     app.enableCors();
@@ -12,4 +12,5 @@ export async function bootstrap() {
     console.log(error);    
   }
 }
-bootstrap();
+// bootstrap();
+export default bootstrap;
